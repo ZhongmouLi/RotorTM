df7e4ee9011baef5457075f652cd9c5ab1ce22da
Zhongmou Thinkpad <robin.zhongmou.li@gmail.com>
2024-09-25 12:07:44 +0100

    fixed bug by clearing net mavs wrench at the end of int

--- rotor_tm_sim/include/rotor_tm_sim/lib_cooperative.hpp
+++ rotor_tm_sim/include/rotor_tm_sim/lib_cooperative.hpp
@@ -21,43 +21,55 @@ class Cooperative{
         size_t number_robots_;
 
         // controller inputs for n MAVs
-        // for each MAV, the control input is thrust (scalar) + torque (3X1 vector)        
-        std::vector<std::pair<double, Eigen::Vector3d>> v_controllers_inputs_;
+        // for each MAV, the control input is wrench        
+        std::vector<ThrustWrench> v_controllers_inputs_;
 
-        // compute the projection of robot vel perpendicular to the cable
-        // input: drone vel (3X1 vector) and cable direction (3X1 vector)
+        Cooperative() =  delete;
+
+        Wrench net_mavs_wrench_to_payload_;
+
+        CooperIntertPara interaction_parameters_;
+
+        void ClearNetWrenches2Payload();
+
     public:
 
-    // put them in private LATER
-    // vector of cable instances
-    std::vector<UAVCable> v_drone_cable_;
+        // put them in private LATER
+        // vector of cable instances
+        std::shared_ptr<Payload> ptr_payload_;
 
-    // payload
-    Payload payload_;
+        std::vector<std::shared_ptr<Joint>> v_ptr_joints_;
 
-    Cooperative(const std::vector<Eigen::Vector3d> &v_attach_point_post_bf, const double &mav_mass, const Eigen::Matrix3d &mav_inertia, const double cable_length, const double &payload_mass, const Eigen::Matrix3d &payload_inertia, const double &step_size);
+        std::vector<std::shared_ptr<UAVCable>> v_ptr_uavcables_;
 
-    // CheckCollisions4MAVsPayload checks if collission happens for every cable
-    // void CheckCollisions4MAVCables();
 
-    // set init post of payload
-    void SetPayloadInitPost();
 
-    void  SetPayloadInitPost(const Eigen::Vector3d &payload_init_post);    
+        Cooperative(const std::shared_ptr<Payload>& ptr_payload, const std::vector<std::shared_ptr<Joint>>&v_ptr_joints, const std::vector<std::shared_ptr<UAVCable>>& v_ptr_uavcables_);
 
-    void SetPayloadInitialAccAndBodyrateACC();
+        Cooperative(const std::shared_ptr<Payload>& ptr_payload, const std::shared_ptr<Joint> &ptr_joint, const std::shared_ptr<UAVCable>& ptr_uavcable_);
 
-    // update vels of collided MAVs and payload 
-    // method: eq (39)-(42)
-    void UpdateVelsCollidedUAVsPayload();
+        // CheckCollisions4MAVsPayload checks if collission happens for every cable
+        // void CheckCollisions4MAVCables();
 
-    void InputControllerInput4MAVs(const Eigen::VectorXd v_mavs_thrust, const std::vector<Eigen::Vector3d> v_mavs_torque);
+        // set init post of payload
+        void SetPayloadInitPost();
 
-    // compute interation wrenches and vars for MAVs and payload
-    void ComputeInteractWrenches();
+        void SetPayloadInitPost(const Eigen::Vector3d &payload_init_post);    
 
-    // call one step dynamic simulation for MAVs and payload
-    void DoOneStepInt4Robots();
+        // void SetPayloadInitialAccAndBodyrateACC();
+        void UpdateJointAndCableStatus();
+
+        // update vels of collided MAVs and payload 
+        // method: eq (39)-(42)
+        void UpdateVelsCollidedUAVsPayload();
+
+        void InputControllerInput4MAVs(const std::vector<double> v_mavs_thrust, const std::vector<Eigen::Vector3d> v_mavs_torque);
+
+        // compute interation wrenches and vars for MAVs and payload
+        void ComputeInteractWrenches();
+
+        // call one step dynamic simulation for MAVs and payload
+        void DoOneStepInt4Robots();
 };
 
 
